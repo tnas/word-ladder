41c155f57b9a6239e4730ffd2b0f8768685532e6
Thiago Nascimento <nascimenthiago@gmail.com>
2020-10-14 12:08:36 -0300

    fixing the files reading

--- src/ang.rs
+++ src/ang.rs
@@ -1,4 +1,4 @@
-pub fn build_ladder(start: String, end: String) {
-    println!("{} - {}", start, end);
+pub fn build_ladder(start: String, end: String, dictionary: Vec<String>) {
+    println!("{} - {} - Number of words: {}", start, end, dictionary.len());
 }
 
